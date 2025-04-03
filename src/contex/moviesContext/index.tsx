f2f03dc661e75dd8d788e309dda657c1fa395a90
Josimar Souza <contact.josimarsouza@gmail.com>
2025-04-03 14:42:02 -0300

    feat: fetching movies when movies context is constructed

--- src/contex/moviesContext/index.tsx
+++ src/contex/moviesContext/index.tsx
@@ -1,14 +1,59 @@
-import { createContext } from "react";
-import { MoviesContextType } from "./moviesContextTypes";
+import {
+  createContext,
+  useEffect,
+  useState,
+  useMemo,
+} from "react";
+import { MoviesContextType, DefaultMoviesType } from "./moviesContextTypes";
+import MoviesAPI from "../../services/APIs/movieAPI";
+import ErrorCreator from "../../helpers/error/errorCreator";
 
 type MoviesProviderType = {
   children: React.ReactNode,
 }
 
 const moviesContext = createContext<MoviesContextType>({});
+const moviesAPI = new MoviesAPI(5000);
 
 function MoviesProvider({ children }: MoviesProviderType) {
-  const contextValues = {};
+  const [defaultMovies, setDefaultMovies] = useState<DefaultMoviesType>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const fetchMovies = async () => {
+      const nowPlaying = await moviesAPI.GetNowPlaying();
+      const popular = await moviesAPI.GetPopular();
+      const topRated = await moviesAPI.GetTopRated();
+      const upcoming = await moviesAPI.GetUpcoming();
+
+      if (
+        !(nowPlaying instanceof ErrorCreator) &&
+        !(popular instanceof ErrorCreator) &&
+        !(topRated instanceof ErrorCreator) &&
+        !(upcoming instanceof ErrorCreator)
+      ) {
+        setDefaultMovies(
+          {
+            nowPlaying,
+            popular,
+            topRated,
+            upcoming,
+          }
+        )
+
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  const contextValues = useMemo(() => ({
+    defaultMovies,
+    isLoading,
+  }), [defaultMovies, isLoading]);
 
   return (
     <moviesContext.Provider value={contextValues}>
--- src/contex/moviesContext/moviesContextTypes.ts
+++ src/contex/moviesContext/moviesContextTypes.ts
@@ -1,1 +1,10 @@
-export type MoviesContextType = {}
+import { MoviesListType } from "../../helpers/types/moviesTypes";
+
+export type MoviesContextType = {};
+
+export type DefaultMoviesType = {
+  nowPlaying: MoviesListType,
+  popular: MoviesListType,
+  topRated: MoviesListType,
+  upcoming: MoviesListType,
+}
