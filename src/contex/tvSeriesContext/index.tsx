d6c3a9379bce76279f6d3c21b6edfe0f7ec30edc
Josimar Souza <contact.josimarsouza@gmail.com>
2025-04-03 15:00:13 -0300

    feat: Created base tv series context and added the provider

--- src/contex/tvSeriesContext/index.tsx
+++ src/contex/tvSeriesContext/index.tsx
@@ -1,14 +1,62 @@
-import { createContext } from "react";
-import { TVSeriesContextType } from "./tvSeriesContextTypes";
+import {
+  createContext,
+  useState,
+  useEffect,
+  useMemo,
+} from "react";
+import {
+  TVSeriesContextType,
+  TVSeriesListsType,
+} from "./tvSeriesContextTypes";
+import TVSeriesAPI from "../../services/APIs/tvSeriesAPI";
+import ErrorCreator from "../../helpers/error/errorCreator";
 
 const tvSeriesContext= createContext<TVSeriesContextType>({});
+const tvSeriesAPI = new TVSeriesAPI(5000);
 
 type TVSeriesProviderType = {
   children: React.ReactNode,
 }
 
 function TVSeriesProvider({ children }: TVSeriesProviderType) {
-  const contextValues = {};
+  const [tvSeriesLists, setTVSeriesLists] = useState<TVSeriesListsType>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const fetchTVSeries = async () => {
+      const airingToday = await tvSeriesAPI.GetAiringToday();
+      const onTheAir = await tvSeriesAPI.GetOnTheAir();
+      const popular = await tvSeriesAPI.GetPopular();
+      const topRated = await tvSeriesAPI.GetTopRated();
+
+      if (
+        !(airingToday instanceof ErrorCreator) &&
+        !(onTheAir instanceof ErrorCreator) &&
+        !(popular instanceof ErrorCreator) &&
+        !(topRated instanceof ErrorCreator)
+      ) {
+        setTVSeriesLists(
+          {
+            airingToday,
+            onTheAir,
+            popular,
+            topRated,
+          }
+        )
+
+        setIsLoading(false);
+      }
+    };
+
+    fetchTVSeries();
+  }, []);
+
+  const contextValues = useMemo(() => ({
+    tvSeriesLists,
+    isLoading,
+  }), [tvSeriesLists, isLoading]);
 
   return (
     <tvSeriesContext.Provider value={contextValues}>
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.tsx'
 
 import MoviesProvider from './contex/moviesContext/index.tsx'
+import TVSeriesProvider from './contex/tvSeriesContext/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MoviesProvider>
-      <App />
+      <TVSeriesProvider>
+        <App />
+      </TVSeriesProvider>
     </MoviesProvider>
   </StrictMode>,
 )
